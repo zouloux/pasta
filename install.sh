02f4d8a5c69eabacea2f827b5013ffef1421d4cd
Alexis Bouhet <alexis@les-hallucines.studio>
2024-06-19 16:18:54 +0200

    New install script

--- install.sh
+++ install.sh
@@ -10,17 +10,65 @@ echo "\_|  \__,_|___/\__\__,_| \____/ \___|_|    \_/ \___|_|"
 echo ""
 
 pastaDir="/usr/local/pasta"
+authorizedKeys="/root/.ssh/authorized_keys"
+sshConfig="/etc/ssh/sshd_config"
 
 if [ "$(id -u)" -ne 0 ]; then
-  echo "Please run as root"
+  echo "Please switch to root user and restart install:"
+  echo "\$ sudo su -"
   exit 1
 fi
 
 # Check if pasta exists
 if [ -d "/usr/local/pasta/" ]; then
   echo "Pasta Server seems to be already installed on this server."
-  echo "Please run update script."
+  echo "Please update with:"
+  echo "\$ pasta-upgrade"
   exit 1
+fi
+
+# Ask to add SSH key
+if [ ! -f "$authorizedKeys" ] || ! grep -q "ssh-" "$authorizedKeys"; then
+  echo ""
+  echo "You should add your public key to login to root."
+  echo "This is highly recommended."
+  read -p "Enter your public SSH key: " sshKey
+  needRestart=false
+  if [ -n "$sshKey" ]; then
+    echo "Installing SSH key..."
+    mkdir -p /root/.ssh
+    echo "$sshKey" >> /root/.ssh/authorized_keys
+    chmod 600 /root/.ssh/authorized_keys > /dev/null 2>&1
+    echo ""
+    echo "For better security, you can now disable your SSH password access."
+    read -p "Do you want to disable SSH password access? (y/n) " disablePasswordAccess
+    if [ "$disablePasswordAccess" == "y" ]; then
+      sed -i "s/^#PasswordAuthentication yes/PasswordAuthentication no/" $sshConfig
+      sed -i "s/^#ChallengeResponseAuthentication yes/ChallengeResponseAuthentication no/" $sshConfig
+      rm -f /etc/ssh/sshd_config.d/50-cloud-init.conf > /dev/null 2>&1
+      needRestart=true
+    fi
+  fi
+  echo ""
+  echo "For better security, you can change your SSH port."
+  echo "Select a port number and do not forget to open this port on firewall!"
+  read -p "Enter a new SSH port (between 2000 and 8000, empty to keep 22): " sshPort
+  if [[ -n "$sshPort" && ( ( $sshPort -lt 2000 || $sshPort -gt 8000 ) && $sshPort -ne 22 ) ]]; then
+    echo "$sshPort not in range, skipping..."
+  elif [ -n "$sshPort" ]; then
+    echo "Saving SSH config..."
+    sed -i "s/^#Port 22/Port $sshPort/" $sshConfig
+    needRestart=true
+  fi
+  if [ "$needRestart" = true ]; then
+    echo "Restarting SSH ..."
+    systemctl restart ssh > /dev/null 2>&1
+    if [ "$sshPort" -ne 22 ]; then
+      echo ""
+      echo "Please reconnect using the new port $sshPort with user root."
+      echo "IMPORTANT : Do not forget to open the port $sshPort on firewall!"
+    fi
+  fi
 fi
 
 # Check if docker is present
@@ -60,39 +108,39 @@ echo ""
 read -p "Enable ASCII banner login showing '$hostname' ? (y/n)" doBanner
 
 # Set the hostname
-echo "Configuring hosts ..."
+echo "Configuring hosts..."
 echo "$hostname" > /etc/hostname
 hostnamectl set-hostname "$hostname" > /dev/null 2>&1
 echo "" >> /etc/hosts
 echo "127.0.0.1 $hostname" >> /etc/hosts
 
 # Update and install core dependencies
-echo "Installing core dependencies ..."
+echo "Installing core dependencies..."
 apt update -qq > /dev/null 2>&1
 apt upgrade -y -qq > /dev/null 2>&1
 apt install git logrotate figlet openssl rsync htop acl -y -qq > /dev/null 2>&1
 
 # Create ASCII banner and remove figlet
-if [ "doBanner" == "y" ]; then
-  echo "Creating login banner ..."
+if [ "$doBanner" == "y" ]; then
+  echo "Creating login banner..."
   banner=$(echo $hostname | figlet -w 120)
   echo "$banner" > /etc/motd
   # Enable banner for SSH login
-  echo "PrintMotd yes" >> /etc/ssh/sshd_config
+  echo "PrintMotd yes" >> $sshConfig
   systemctl restart sshd > /dev/null 2>&1
 fi
 apt remove figlet -y -qq > /dev/null 2>&1
 
 # Install Docker
 if ! command -v docker &> /dev/null; then
-  echo "Installing docker ..."
+  echo "Installing docker..."
   curl -fsSL https://get.docker.com -o get-docker.sh > /dev/null 2>&1
   sh get-docker.sh -y > /dev/null 2>&1
   rm get-docker.sh
 fi
 
 # Install lazydocker
-echo "Installing lazydocker ..."
+echo "Installing lazydocker..."
 curl -fsSL https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh -o install-lazy-docker.sh > /dev/null 2>&1
 bash install-lazy-docker.sh > /dev/null 2>&1
 rm install-lazy-docker.sh
@@ -100,7 +148,7 @@ mv .local/bin/lazydocker /usr/bin
 rm -rf .local
 
 # Configure logs
-echo "Configuring logs ..."
+echo "Configuring logs..."
 echo "include /etc/logrotate.d" > /etc/logrotate.conf
 # Add a logrotate configuration for all logs
 cat <<EOF > /etc/logrotate.d/all_logs
@@ -128,14 +176,14 @@ EOF
 systemctl restart systemd-journald > /dev/null 2>&1
 
 # Save config
-echo "Creating directories ..."
+echo "Creating directories..."
 cd /root
 mkdir -p containers/apps/
 mkdir -p containers/services/proxy/
 mkdir -p containers/projects/
 
 # Save config
-echo "Saving config ..."
+echo "Saving config..."
 mkdir -p "$pastaDir/config"
 cd "$pastaDir/config"
 echo "$hostname" > hostname.txt
@@ -144,21 +192,22 @@ echo "$adminEmail" > email.txt
 cd /root
 
 # Clone repo
-echo "Cloning Pasta repo ..."
+echo "Cloning Pasta repo..."
+rm -rf /tmp/pasta > /dev/null 2>&1
 git clone https://github.com/zouloux/pasta.git /tmp/pasta > /dev/null 2>&1
 
-echo "Installing pasta bin ..."
+echo "Installing pasta bin..."
 mkdir -p "$pastaDir" > /dev/null 2>&1
-cp -r "/tmp/pasta/server/pasta/*" "$pastaDir" > /dev/null 2>&1
+cp -r /tmp/pasta/server/pasta/* "$pastaDir" > /dev/null 2>&1
 
-echo "Configuring bashrc ..."
+echo "Configuring bashrc..."
 mv /root/.bashrc /root/.bashrc.old > /dev/null 2>&1
 cp /tmp/pasta/server/.bashrc "$pastaDir" > /dev/null 2>&1
 chmod 0750 "$pastaDir/.bashrc" > /dev/null 2>&1
 echo "source $pastaDir/.bashrc" > /root/.bashrc
 source /root/.bashrc
 
-echo "Setting up nginx proxy ..."
+echo "Setting up nginx proxy..."
 cp /tmp/pasta/server/containers/services/proxy/docker-compose.yaml /root/containers/services/proxy/
 cp -r /tmp/pasta/server/containers/services/proxy/config/ /root/containers/services/proxy/config/
 
@@ -166,14 +215,14 @@ echo "Configuring deployment group..."
 groupadd pasta > /dev/null 2>&1
 echo "%pasta ALL=(ALL) NOPASSWD: /usr/local/pasta/bin/project-deploy" >> /etc/sudoers.d/pasta-deploy
 
-echo "Creating docker network ..."
+echo "Creating docker network..."
 docker network create pasta > /dev/null 2>&1
 
-echo "Downloading proxy image ..."
+echo "Downloading proxy image..."
 cd /root/containers/services/proxy/
 docker compose build > /dev/null 2>&1
 
-echo "Starting proxy ..."
+echo "Starting proxy..."
 docker compose up -d > /dev/null 2>&1
 cd /root
 
